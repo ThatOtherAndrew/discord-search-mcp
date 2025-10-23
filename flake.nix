bce88fc50f64ab5c17cf735e2c986c0a91488db8
Andromeda <stroev.andrew@gmail.com>
2025-10-23 19:26:59 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Discord Search MCP";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs: let
+    system = "x86_64-linux";
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "discord-search-mcp";
+
+      packages = with pkgs; [
+        uv
+      ];
+    };
+  };
+}
---
+++ src/discord-search-mcp/__main__.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    print("Hello from discord-search-mcp!")
+    print(sys.version)
+
+
+if __name__ == "__main__":
+    main()
